32c5e3493e3cf3e8f361bcb98d9daea8ac2f2096
N.SH <sky.blush@ya.ru>
2022-04-28 15:13:13 +0300

    add index router

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const auth = require('./middlewares/auth');
-const { createUser, login, logout } = require('./controllers/users');
-const { validateRegister, validateLogin } = require('./middlewares/validation');
-const NotFoundError = require('./errors/NotFoundError');
+const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -33,20 +30,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.post('/signup', validateRegister, createUser);
-app.post('/signin', validateLogin, login);
-app.post('/signout', logout);
-
 app.use(cookieParser());
 app.use(helmet());
-app.use(auth);
 
-app.use('/users', require('./routes/users'));
-app.use('/movies', require('./routes/movies'));
-
-app.use(() => {
-  throw new NotFoundError('Страница не найдена');
-});
+app.use(router);
 
 app.use(errorLogger);
 
